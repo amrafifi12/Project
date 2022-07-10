8a3c4a0a809b98231354d2143ea039aac334645e
amrafifi11111@outlook.sa <amrafifi11111@outlook.sa>
2022-07-10 21:30:08 +0200

    new

---
+++ Project/Models/Exam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Exam
+    {
+        public int Id { set; get; }
+
+        //
+        public int CourseId { set; get; }
+
+        //
+        public int InstructorId { set; get; }
+        public DateTime Start { set; get; }
+        public DateTime End { set; get; }
+        public int degree { set; get; }
+        public List<ExamQuestions> ExamQuestions { set; get; }
+
+    }
+}
---
+++ Project/Models/ExamQuestions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class ExamQuestions
+    {
+        [Key][Column(Order =0)]
+        public int QuestID { set; get; }
+
+        [Key][Column(Order = 1)][ForeignKey("Exam")]
+        public int ExamID { set; get; }
+        public virtual Exam Exam { set; get; }
+    }
+}
